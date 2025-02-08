23eca390de9ea58cf61d170538ef87692c7dd19e
amirsakrGH <amirsakr001@gmail.com>
2025-02-08 22:53:50 +0200

    d

---
+++ Demo/Utilities/Utility.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Exams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Utilities
+{
+    internal static class Utility
+    {
+        //static public TimeSpan TimeLeft { get; set; }
+        //static public DateTime startTime = DateTime.Now;
+        //static public bool TimeUp(byte Time)
+        //{
+        //    TimeSpan x = DateTime.Now - startTime;
+        //    TimeLeft = x.TotalMinutes;
+        //    return x.TotalMinutes >= Time;
+
+        //}
+    }
+}
